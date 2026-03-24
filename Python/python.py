f6641b287137f63fa5bd5431e3cd660862b737c0
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2026-03-24 09:52:22 +0200

    Update python.py

--- Python/python.py
+++ Python/python.py
@@ -2,13 +2,11 @@ import urllib.request
 import random
 username = 'USERNAME'
 password = 'PASSWORD'
-country = 'US'
-session = random.random()
-entry = ('http://customer-%s-cc-%s:%s@pr.oxylabs.io:7777' %
-    (username, country, password))
+entry = ('http://customer-%s:%s@pr.oxylabs.io:7777' %
+    (username, password))
 query = urllib.request.ProxyHandler({
     'http': entry,
     'https': entry,
 })
 execute = urllib.request.build_opener(query)
-print(execute.open('https://ipinfo.io').read())
+print(execute.open('https://ip.oxylabs.io/location').read())
