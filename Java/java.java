d142f58cffaed9cae98da77a3710c4b897d07a97
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2026-03-24 09:51:18 +0200

    Update java.java

--- Java/java.java
+++ Java/java.java
@@ -7,8 +7,8 @@ public class Example {
     public static void main(String[] args) throws Exception {
         HttpHost entry = new HttpHost("pr.oxylabs.io", 7777);
         String query = Executor.newInstance()
-            .auth(entry, "customer-USERNAME-cc-US", "PASS")
-            .execute(Request.Get("http://icanhazip.com").viaProxy(entry))
+            .auth(entry, "customer-USERNAME", "PASS")
+            .execute(Request.Get("http://ip.oxylabs.io/location").viaProxy(entry))
             .returnContent().asString();
         System.out.println(query);
     }
